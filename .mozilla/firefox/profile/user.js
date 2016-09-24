4ce5a2b41d48ceb2a976635a646c95fc659e25c3
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2016-09-24 11:44:44 -0300

    Added firefox user config.

---
+++ .mozilla/firefox/profile/user.js
@@ -0,0 +1,6 @@
+// Disable automatic updates
+// It can't really update automatically...
+user_pref("app.update.auto", false);
+
+// Enable touch scroll iteraction
+user_pref("dom.w3c_touch_events.enabled", 1);
