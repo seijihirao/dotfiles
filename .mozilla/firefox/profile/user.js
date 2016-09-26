602c8d42d3eb4031f48d0e548b0a2accb2289790
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2016-09-25 23:14:23 -0300

    Removed firefox update.

--- .mozilla/firefox/profile/user.js
+++ .mozilla/firefox/profile/user.js
@@ -1,6 +1,8 @@
 // Disable automatic updates
 // It can't really update automatically...
+user_pref("app.update.enabled", false);
 user_pref("app.update.auto", false);
+user_pref("app.update.silent", false);
 
 // Enable touch scroll iteraction
 user_pref("dom.w3c_touch_events.enabled", 1);
