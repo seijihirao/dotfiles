dde36fdca6dab8691ad6b572114b675f62d4e121
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2016-10-07 12:15:51 -0300

    Updated bash configs.

--- .config/bash/alias.sh
+++ .config/bash/alias.sh
@@ -3,3 +3,4 @@ shopt -s expand_aliases
 alias cp="cp -i"                          # confirm before overwriting something
 alias df='df -h'                          # human-readable sizes
 alias free='free -h'                      # show sizes in MB
+alias clipboard='xclip -selection clipboard' # copy to clipboard
