fc72f8df0d4a363491d0c15a6ec3bfda48081fe0
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2016-10-26 11:38:10 -0200

    Updated bash_history behavior.

--- .config/bash/bash.sh
+++ .config/bash/bash.sh
@@ -1,2 +1,6 @@
-#Remove repeated commands on bash history
-export HISTCONTROL=ignoreboth:erasedups
+# Better bash_history
+export HISTFILESIZE=1000000
+export HISTSIZE=1000000
+export HISTCONTROL=ignoreboth # remove repeated commands on bash history
+export HISTIGNORE='cd:ls:bg:fg:history'
+export PROMPT_COMMAND='history -a' # add to history right when command is used (not working :/)
