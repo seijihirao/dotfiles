95880945a57987128af1e20b99ae48032675ed44
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2016-11-03 22:35:29 -0200

    Organized aliases.

--- .config/bash/alias.sh
+++ .config/bash/alias.sh
@@ -1,6 +1,18 @@
 shopt -s expand_aliases
 
+#
+# System
+#
+alias df='df -h'                          # sidk in human-readable sizes
+alias free='free -h'                      # memory in human-readable sizes
+
+#
+# Files
+#
 alias cp="cp -i"                          # confirm before overwriting something
-alias df='df -h'                          # human-readable sizes
-alias free='free -h'                      # show sizes in MB
 alias clipboard='xclip -selection clipboard' # copy to clipboard
+
+#
+# Programming
+#
+alias emacs="emacs -nw"
