b09043fc512caaedb855d3ed096b06dc8fcfd64f
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2017-01-13 14:44:35 -0200

    Updated node path.

--- .config/bash/programming/node.sh
+++ .config/bash/programming/node.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 # NVM
-export NVM_DIR="/home/seiji/.nvm"
+export NVM_DIR="/home/seiji/.config/programming/node/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-
-export PATH="$PATH:~/.npm/bin"
