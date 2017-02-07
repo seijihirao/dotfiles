242b00db57909cfff091e3b5219024832fc57110
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2017-02-07 17:35:38 -0200

    "Optimized" nvm startup.

--- .config/bash/programming/node.sh
+++ .config/bash/programming/node.sh
@@ -2,4 +2,4 @@
 
 # NVM
 export NVM_DIR="/home/seiji/.config/programming/node/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" &  # This loads nvm
