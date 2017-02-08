512efbfbd8d2c95165c4d3cdb74c9b239bce6355
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2017-02-08 16:40:43 -0200

    Updated git on ps1.

--- .config/bash/ps.sh
+++ .config/bash/ps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# PS
+# COLORS
 #
 RESET="\[\e[0m\]"
 BOLD="\[\e[1m\]"
@@ -11,8 +11,21 @@ YELLOW="\[\e[33m\]"
 BLUE="\[\e[34m\]"
 WHITE="\[\e[39m\]"
 
+#
+# GIT
+#
+export GIT_PS1_DESCRIBE_STYLE='contains'
+export GIT_PS1_SHOWCOLORHINTS=1
+export GIT_PS1_SHOWDIRTYSTATE=1
+export GIT_PS1_SHOWSTASHSTATE=1
+#export GIT_PS1_SHOWUNTRACKEDFILES=1
+export GIT_PS1_SHOWUPSTREAM=1
+
+#
+# PS
+#
 update-ps() {
-    export PS1="$BOLD$GREEN$(__git_ps1) $BLUE[\u@\h $WHITE\W$BLUE]\$\[$(tput sgr0)\]$RESET "
+    export PS1="$BOLD$GREEN$(__git_ps1 '(%s)')$BLUE[\u@\h $WHITE\W$BLUE]\$\[$(tput sgr0)\]$RESET "
     export PS2="$BOLD$BLUE>$RESET "
 }
-export PROMPT_COMMAND=update-ps
+export PROMPT_COMMAND="update-ps;$PROMPT_COMMAND"
