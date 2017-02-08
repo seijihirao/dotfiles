a27b1dd29c421ea0d1b3cc3c2b37a28d13594f7c
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2017-02-08 16:06:53 -0200

    Updated ps1 colors.

--- .config/bash/ps.sh
+++ .config/bash/ps.sh
@@ -12,7 +12,7 @@ BLUE="\[\e[34m\]"
 WHITE="\[\e[39m\]"
 
 update-ps() {
-    export PS1="$BOLD$BLUE[\u@\h $WHITE\W$BLUE]$GREEN$(__git_ps1)$BLUE \$\[$(tput sgr0)\]$RESET "
+    export PS1="$BOLD$GREEN$(__git_ps1) $BLUE[\u@\h $WHITE\W$BLUE]\$\[$(tput sgr0)\]$RESET "
     export PS2="$BOLD$BLUE>$RESET "
 }
 export PROMPT_COMMAND=update-ps
