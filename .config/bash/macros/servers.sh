1972c76039f6e8daba219dd8544082e290a34100
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2017-03-22 21:13:54 -0300

    Added scripts to ssh on mfab and ba.

---
+++ .config/bash/macros/servers.sh
@@ -0,0 +1,40 @@
+#
+# General
+#
+
+ssh-gen(){
+    ssh "$1@$2"
+}
+
+sshfs-gen(){
+    dir="/media/$1/"
+    if [ ! -d "${dir}" ]; then
+        mkdir "${dir}"
+    fi
+    chown "${USER}" "${dir}"
+    sshfs "$2" "${dir}"
+}
+
+#
+# Rep MFAB
+#
+
+ssh-mfab(){
+    ssh-gen mfab 192.168.1.113
+}
+
+sshfs-mfab(){
+    sshfs-gen mfab root@192.168.1.113:/media
+}
+
+#
+# Bacchan
+#
+
+ssh-mfab(){
+    ssh-gen ${USER} 192.168.1.10
+}
+
+sshfs-ba(){
+    sshfs-gen ba 192.168.1.10:/media 
+}
