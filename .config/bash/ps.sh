e01a049c4d8588e0d4c6fec850cab791e2aa2695
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2017-04-06 20:08:39 -0300

    Added powerline.
    Still with line wrapping bug.

--- .config/bash/ps.sh
+++ .config/bash/ps.sh
@@ -6,10 +6,14 @@
 RESET="$(tput sgr0)"
 BOLD="$(tput bold)"
 
+BLACK="$(tput setaf 0)"
 GREEN="$(tput setaf 2)"
 YELLOW="$(tput setaf 3)"
 BLUE="$(tput setaf 4)"
 WHITE="$(tput setaf 7)"
+
+BG_WHITE="$(tput setab 7)"
+BG_BLUE="$(tput setab 4)"
 
 #
 # GIT
@@ -25,7 +29,9 @@ export GIT_PS1_SHOWUPSTREAM=1
 # PS
 #
 update-ps() {
-    export PS1="\[${BOLD}${GREEN}\]$(__git_ps1 '(%s)')\[${BLUE}\][\u@\h \[${WHITE}\]\W\[${BLUE}\]]\$\[${RESET}\] "
-    export PS2="\[${BOLD}${BLUE}\]>\[${RESET}\] "
+    export PS1="$(powerline shell aboveleft)"
+    #export PS1="\[${BOLD}${BG_WHITE}${BLACK}\]$(__git_ps1 ' %s ')\[${RESET}${BOLD}${BG_BLUE}${WHITE}\][\u@\h \[${WHITE}\]\W\[${BLUE}\]]\$\[${RESET}\] "
+    export PS2="$(powerline shell left)"
+    #export PS2="\[${BOLD}${BLUE}\]>\[${RESET}\] "
 }
 export PROMPT_COMMAND="update-ps;$PROMPT_COMMAND"
