1d8a2258b4778f5b5ddf341b0362b78e37e57c2d
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2017-07-06 10:54:46 -0300

    Updated n path.

--- .config/bash/programming/node.sh
+++ .config/bash/programming/node.sh
@@ -5,5 +5,5 @@ export NPM_CONFIG_PREFIX=~/.config/programming/npm
 export PATH=$PATH:~/.config/programming/npm/bin
 
 # N - version manager
-export N_PREFIX=~/.config/programming/n
+export N_PREFIX="$HOME/.config/programming/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
 
