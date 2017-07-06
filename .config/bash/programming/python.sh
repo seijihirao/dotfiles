8a13c479464ef556cac0fb5dbb9b88989ad9cc08
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2017-07-06 10:54:36 -0300

    Updated python-venv bash function.
    Now using getopt.

--- .config/bash/programming/python.sh
+++ .config/bash/programming/python.sh
@@ -5,18 +5,55 @@
 #
 
 python-venv(){
-    local venvpath=".venv"
- 
-    if ! [ -z ${1} ]; then
-        venvpath="${1}"
+    ARGS=$(getopt -o ":p:v:" -l "path:,version:" -n "python-venv" -- "$@");
+
+    if [ $? -ne 0 ] ; then
+      return 1
     fi
+
+    eval set -- "$ARGS";
     
+    local venvpath=".venv"
+    local pythonversion
+    
+    while true ; do
+        case "$1" in
+            -p|--path)
+                shift;
+                if [ -n "$1" ]; then
+                    venvpath="$1";
+                fi
+                shift;
+                ;;
+            -v|--version)
+                shift;
+                if [ -n "$1" ]; then
+                    pythonversion="$1";
+                fi
+                shift;
+                ;;
+            --)
+                shift;
+                break;
+                ;;
+            *)
+                echo $1;
+                shift;
+                ;;
+        esac
+    done
+
     if ! [ -d ${venvpath} ]; then
         printf "Directory \"${venvpath}\" does not exist.\n"
         read -p "Do you want to create a new environment? (Y/n)" createvenv
         case ${createvenv} in
             [Nn]* ) return;;
-            * ) virtualenv ${venvpath};;
+            * ) if [ -z "${pythonversion}" ]; then
+                    virtualenv ${venvpath};
+                else
+                    virtualenv "--python=python${pythonversion}" ${venvpath};
+                fi
+
         esac
     fi
     . ${venvpath}/bin/activate
